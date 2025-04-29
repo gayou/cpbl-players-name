506d682bcd46d1b76b14b55c4dd448997bb1a14d
gayou <gayou.ura@gmail.com>
2025-04-29 09:45:39 +0900

    ユニットテスト

---
+++ tests/CpblPlayersNameTest.php
@@ -0,0 +1,38 @@
+<?php namespace Gayou\CpblPlayersName\Test;
+
+use PHPUnit\Framework\TestCase;
+use Gayou\CpblPlayersName\CsvDataCreator;
+use Gayou\CpblPlayersName\CpblPlayersName;
+
+class CpblPlayersNameTest extends TestCase
+{
+
+    public static function setUpBeforeClass(): void
+    {
+        CsvDataCreator::setup();
+        CpblPlayersName::init();
+    }
+
+    /**
+     * 選手名のローマ字表記を取得できること
+     *
+     */
+    public function testRomanize()
+    {
+        // 陳佳樂
+        $this->assertSame(CpblPlayersName::romanize('陳佳樂'), 'CHEN Jia Le');
+
+        // 二宮衣沙貴
+        $this->assertSame(CpblPlayersName::romanize('二宮衣沙貴'), 'NINOMIYA Isaki');
+    }
+
+    /**
+     * CPBLに在籍していない選手はnullが返ってくる
+     *
+     */
+    public function testNoRoasterPlayer()
+    {
+        // 大谷翔平
+        $this->assertSame(CpblPlayersName::romanize('大谷翔平'), null);
+    }
+}
